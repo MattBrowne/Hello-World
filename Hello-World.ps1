b641b82bbc7eb160b166a09e9e6312c1d6e99949
Matt Browne <matt@matthewbrowne.com>
2015-11-27 21:26:28 +0000

    Help Comment
    Added the Help Comments to the file

---
+++ Hello-World.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+
+Just a Hello-World Script
+
+.DESCRIPTION
+
+This is a test script that is just used for playing with stuff, and Git Hub etc.
+
+.PARAMETER computername
+
+Here, the dotted keyword is followed by a single parameter name. Don't precede that with a hyphen. The following lines describe the purpose of the parameter:
+.PARAMETER filePath
+
+Provide a PARAMETER section for each parameter that your script or function accepts.
+.EXAMPLE
+
+Just run it.
+.EXAMPLE
+Really just run the thing....
+#>
+
+
+write-host "Hello-World"
